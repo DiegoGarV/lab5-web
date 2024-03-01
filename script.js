f1e7d18d82a7f58ba1ba5764280f6f1969366887
Diego <dgv3105@gmail.com>
2024-03-01 15:02:39 -0600

    Commit 3

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 //Crea el contenedor para todos los textos del chat
-let contenedor = document.createElement('div');
+const contenedor = document.createElement('div');
 contenedor.id = 'contenedor';
 
 //Crea el espacio para el listado de personas en el chat
-let listadoPersonas = document.createElement('div');
+const listadoPersonas = document.createElement('div');
 listadoPersonas.id = 'listado-chats';
-listadoPersonas.textContent = 'listado de integrantes';
+
+// Integrantes en la API
+let integrantes = {};
+const usuario = "FBI";
 
 //Crea el espacio para agregar cada perfil individual
-let contenedorIntegrante = document.createElement("div");
-
-//Agrega la foto de perfil de los integrantes
-let imagenPerfilIntegrante = document.createElement('img');
-
-//Agrega el nombre de los integrantes
-let namePerfilIntegrante = document.createElement('text');
-namePerfilIntegrante.textContent = 'Generico';
+let contenedorIntegrante;
 
 //Crea el espacio para ver el contenido del chat
-let contenidoChat = document.createElement('div');
+const contenidoChat = document.createElement('div');
 contenidoChat.id = 'mensajes';
 
+// Crea la barra de búsqueda
+const barraBusqueda = document.createElement('input');
+barraBusqueda.type = 'text';
+barraBusqueda.id = 'barra-busqueda';
+barraBusqueda.placeholder = 'Buscar mensaje...';
+
 //Auto scroll para los chats
-let contenedorScroll = document.getElementById('mensajes');
+const contenedorScroll = document.getElementById('mensajes');
 
 //Define los mensajes recibidos
 let chat;
@@ -31,11 +33,11 @@ let chat;
 let mensajeEnviado;
 
 //Crea el espacio para el nombre y la imagen del perfil
-let contenidoPerfil = document.createElement('div');
+const contenidoPerfil = document.createElement('div');
 contenidoPerfil.id = 'contenido-perfil';
 
 //Coloca la foto de perfil
-let imagenPerfil = document.createElement('img');
+const imagenPerfil = document.createElement('img');
 imagenPerfil.width = '65';
 imagenPerfil.height = '65';
 imagenPerfil.className = 'imagen-perfil';
@@ -43,24 +45,24 @@ imagenPerfil.style.borderRadius = '50%';
 imagenPerfil.src = 'https://i.kym-cdn.com/photos/images/newsfeed/002/546/909/683.jpg';
 
 //Coloca el nombre del perfil
-let namePerfil = document.createElement('h3');
-namePerfil.textContent = 'Rafa P';
+const namePerfil = document.createElement('h3');
+namePerfil.textContent = usuario;
 
 //Crea el botón para cambiar entre modo oscuro y claro
-let botonModo = document.createElement('button');
+const botonModo = document.createElement('button');
 botonModo.textContent = 'Oscuro';
 
 //Crea el espacio para el mensaje que se escribe
-let mensaje = document.createElement('div');
+const mensaje = document.createElement('div');
 mensaje.id = 'contenido-mensaje';
 
 //Área de escritura
-let escribirMensaje = document.createElement('textarea');
+const escribirMensaje = document.createElement('textarea');
 escribirMensaje.id = 'mensaje-chat';
 escribirMensaje.maxLength = 140;
 
 //Botón para enviar
-let botonEnviar = document.createElement('button');
+const botonEnviar = document.createElement('button');
 botonEnviar.textContent = 'Enviar';
 
 //Estilo del contenedor
@@ -69,7 +71,7 @@ contenedor.style.height = '97vh';
 contenedor.style.display = 'grid';
 contenedor.style.fontFamily = 'Times New Roman';
 contenedor.style.border = '1px solid black';
-contenedor.style.gridTemplateColumns = '20% 80%';
+contenedor.style.gridTemplateColumns = '25% 75%';
 contenedor.style.gridTemplateRows = '85% 15%';
 
 //Estilo del listado de personas
@@ -81,19 +83,11 @@ listadoPersonas.style.flexDirection = 'column';
 listadoPersonas.style.overflowY = 'scroll';
 listadoPersonas.style.overflowX = 'hidden';
 
-//Estilo del espacio individual del integrante
-contenedorIntegrante.style.width = "100%";
-contenedorIntegrante.style.minHeight = "60px";
-contenedorIntegrante.style.padding = "5px";
-contenedorIntegrante.style.display = 'flex';
-contenedorIntegrante.style.alignItems = 'center';
-
-//Estilo de la foto de perfil del integrante
-imagenPerfilIntegrante.width = '30';
-imagenPerfilIntegrante.height = '30';
-imagenPerfilIntegrante.style.borderRadius = '50%';
-imagenPerfilIntegrante.style.marginRight = '8px';
-imagenPerfilIntegrante.src = 'https://i.kym-cdn.com/photos/images/newsfeed/002/546/909/683.jpg';
+// Estilo de la barra de busqueda
+barraBusqueda.style.width = '60%';
+barraBusqueda.style.marginLeft = '35px';
+barraBusqueda.style.position = 'fixed';
+barraBusqueda.style.top = '10'; 
 
 //Estilo del perfil
 contenidoPerfil.style.backgroundColor = '#ff6600';
@@ -130,7 +124,7 @@ escribirMensaje.style.width = '93%'
 escribirMensaje.style.backgroundColor = 'white';
 
 //Estilo del botón para enviar mensaje
-botonEnviar.style.backgroundColor = '##D5DBDB';
+botonEnviar.style.backgroundColor = '#D5DBDB';
 
 //Agrega los textos
 document.body.appendChild(contenedor);
@@ -138,126 +132,213 @@ contenedor.appendChild(listadoPersonas);
 contenedor.appendChild(contenidoChat);
 contenedor.appendChild(contenidoPerfil);
 contenedor.appendChild(mensaje);
-listadoPersonas.appendChild(contenedorIntegrante);
-contenedorIntegrante.appendChild(imagenPerfilIntegrante);
-contenedorIntegrante.appendChild(namePerfilIntegrante);
 contenidoPerfil.appendChild(imagenPerfil);
 contenidoPerfil.appendChild(namePerfil);
 contenidoPerfil.appendChild(botonModo);
 mensaje.appendChild(escribirMensaje);
 mensaje.appendChild(botonEnviar);
 
-let data = fetch('https://jsonplaceholder.typicode.com/posts',
-{
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body : JSON.stringify({})
-})    
-function optenerPost2(){
-    let posts = fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
-}
+// Chats en API
+async function obtenerPosts(){
+    try {
+        const data = await fetch('http://uwu-guate.site:3000/messages', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
 
-// ASYNC AWAIT - ME PERMITE ESPERAR LA RESPUESTA DE UNA PETICION ASINCRONA
-async function optenerPosts(){
-    let data = await fetch('https://jsonplaceholder.typicode.com/posts',
-    {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
+        if (!data.ok) {
+            throw new Error(`HTTP error! status: ${data.status}`);
         }
-    })    
-    console.log("await", data);
-    let posts = await data.json();
-    console.log(posts);
 
-    // console.log("string", JSON.stringify(posts));
-    // let new_json = JSON.parse(JSON.stringify(posts));
-    // console.log("new_json", new_json);
-    return posts;
+        const posts = await data.json();
+        console.log(posts);
+
+        return posts;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return [];
+    }
 }
 
-function crearChat(texto, id){
+// Muestra a todos los integrantes
+async function mostrarIntegrantes() {
+    try {
+        // Obtener los posts de la API
+        let misIntegrantes = await obtenerPosts();
+
+        listadoPersonas.innerHTML = ''; 
+
+        // Si es un array, mostrar los integrantes
+        if (listadoPersonas != null) {
+            // Conjunto para realizar un seguimiento de nombres de usuario únicos
+            let nombresUnicos = new Set();
+
+            // Recorre los posts y crea un div para cada integrante
+            misIntegrantes.forEach(post => {
+                // Verifica si el nombre de usuario ya se agregó
+                if (!nombresUnicos.has(post.username)) {
+                    contenedorIntegrante = document.createElement('div');
+                    contenedorIntegrante.className = 'integrantes';
+                    contenedorIntegrante.id = 'integrante';
+                    contenedorIntegrante.style.height = '40px';
+                    contenedorIntegrante.style.borderBottom = '1px solid black';
+                    contenedorIntegrante.style.padding = '8px';
+                    contenedorIntegrante.style.display = 'flex';
+                    contenedorIntegrante.style.alignItems = 'center';
+
+                    contenedorIntegrante.textContent = post.username;
+                    listadoPersonas.appendChild(contenedorIntegrante);
+                    nombresUnicos.add(post.username);
+                }
+            });
+        }
+    } catch (error) {
+        console.error('Error al mostrar los integrantes:', error);
+    }
+}
+
+function crearChat(id, username, content){
     let nuevoChat = document.createElement("div");
     nuevoChat.className = "chat";
     nuevoChat.id = id;
-    nuevoChat.innerText = texto;
+    if(verificarURL(content)){
+        nuevoChat.innerHTML = `<strong>${username}</strong>:<br> <img src="${content}" width="200px"/>`;
+    } else {
+        nuevoChat.innerHTML = `<strong>${username}</strong>:<br> ${content}`;
+    }
     return nuevoChat;
 }
 
-async function crearMensajes(){
-    // mando a traer los post dummys a una api con get
-    let misPosts =  await optenerPosts();
-    
-    // mando a traer el div donde quiero poner los chats
-    if(contenidoChat != null){
-        // transformamos los dicccionarios a un div de chat
-        misPosts.map(post=>{
-            let nuevoChat = crearChat(post.title, post.id);
-            return nuevoChat;
-        })
-        // recorremos los nuevos chats y los agremos al div de listados
-        .forEach(element => {
-            //Crea los mensajes dentro del chat
-            chat = document.createElement('div')
-            chat.className = 'mensaje-chat';
-            chat.id = 'chat';
-            chat.style.backgroundColor = 'lightblue';
-            chat.style.width = '50%';
-            chat.style.minHeight = '40px';
-            chat.style.borderRadius = '8px';
-            chat.style.border = '1px solid black';
-            chat.style.padding = '8px'
-            chat.style.marginBottom = '8px';
+async function mostrarChats(){
+    try {
+        // Obtener los posts de la API
+        let misPosts = await obtenerPosts();
 
-            chat.appendChild(element);
-            contenidoChat.appendChild(chat);
-        });
-        if (contenidoChat !== null) {
-            contenidoChat.scrollTop = contenidoChat.scrollHeight;
+        // Filtrar los mensajes según la búsqueda
+        misPosts = filtrarMensajes(misPosts);
+        
+        // Limpiar el contenidoChat antes de mostrar los mensajes
+        contenidoChat.innerHTML = ''; 
+        contenidoChat.appendChild(barraBusqueda);
+        
+        if(contenidoChat != null){
+            misPosts.map(post =>{
+                let nuevoChat = crearChat(post.id, post.username, post.content);
+                return nuevoChat;
+            })
+            .forEach(element => {
+                //Crea los mensajes dentro del chat
+                chat = document.createElement('div')
+                chat.className = 'mensaje-chat';
+                chat.id = 'chat';
+                chat.style.backgroundColor = 'lightblue';
+                chat.style.width = '50%';
+                chat.style.minHeight = '40px';
+                chat.style.borderRadius = '8px';
+                chat.style.overflowWrap = 'break-word';
+                chat.style.border = '1px solid black';
+                chat.style.padding = '8px'
+                chat.style.marginBottom = '8px';
+
+                // Mira si el mensaje fue mandado por el usuario de este dispositivo
+                if (element.textContent.includes(usuario)) {
+                    chat.style.backgroundColor = 'lightgreen';
+                    chat.style.marginLeft = '470px';
+
+                } else {
+                    chat.style.backgroundColor = 'lightblue';
+                }
+
+                chat.appendChild(element);
+                contenidoChat.appendChild(chat);
+            });
+            if (contenidoChat !== null) {
+                contenidoChat.scrollTop = contenidoChat.scrollHeight;
+            }
         }
+    } catch(error) {
+        console.error('Error al mostrar los chats:', error);
     }
 }
 
-
-
 //Función para mandar el mensaje
-function enviarMensaje(){
-    let contenidoMensaje = document.getElementById('mensaje-chat').value;
+async function enviarMensaje(){
+    try {
+        let contenidoMensaje = document.getElementById('mensaje-chat').value;
 
-    mensajeEnviado = document.createElement('div');
-    mensajeEnviado.className = 'mensaje-enviado';
-    if (!modoClaro) {
-        mensajeEnviado.style.backgroundColor = '#1D8348';
-    }
-    else{
-        mensajeEnviado.style.backgroundColor = 'lightgreen';
-    }
-    mensajeEnviado.style.width = '50%';
-    mensajeEnviado.style.minHeight = '40px';
-    mensajeEnviado.style.borderRadius = '8px';
-    mensajeEnviado.style.border = '1px solid black';
-    mensajeEnviado.style.padding = '8px';
-    mensajeEnviado.style.marginBottom = '8px';
-    mensajeEnviado.innerHTML = contenidoMensaje;
+        let mensajeParaEnviar = {
+            "username": usuario,
+            "message": contenidoMensaje
+        }
+
+        let post = await enviarMensajeApi(mensajeParaEnviar);
+
+        // Crea el mensaje dentro del chat
+        mensajeEnviado = document.createElement('div');
+        mensajeEnviado.className = 'mensaje-enviado';
+
+        if (!modoClaro) {
+            mensajeEnviado.style.backgroundColor = '#1D8348';
+        }
+        else{
+            mensajeEnviado.style.backgroundColor = 'lightgreen';
+        }
+        mensajeEnviado.style.width = '50%';
+        mensajeEnviado.style.minHeight = '40px';
+        mensajeEnviado.style.borderRadius = '8px';
+        mensajeEnviado.style.border = '1px solid black';
+        mensajeEnviado.style.padding = '8px';
+        mensajeEnviado.style.marginBottom = '8px';
+        mensajeEnviado.style.marginLeft = '470px';
+
+        if(verificarURL(contenidoMensaje)){
+            mensajeEnviado.innerHTML = `<strong>${usuario}</strong>:<br> <img src="${contenidoMensaje}" width="200px"/>`;
+        } else {
+            mensajeEnviado.innerHTML = `<strong>${usuario}</strong>:<br>${contenidoMensaje}`;
+        }
+        
+        if (!modoClaro) {
+            mensajeEnviado.classList.add('oscuro');
+        }
     
-    if (!modoClaro) {
-        mensajeEnviado.classList.add('oscuro');
+        contenidoChat.appendChild(mensajeEnviado)
+    
+        animacionEntrada(mensajeEnviado,0);
+    
+        if (contenidoChat !== null) {
+            contenidoChat.scrollTop = contenidoChat.scrollHeight;
+        }
+        escribirMensaje.value = '';
+
+    } catch (error) {
+        console.error('Error al enviar el mensaje:', error);
     }
+}
 
-    contenidoChat.appendChild(mensajeEnviado)
-
-    animacionEntrada(mensajeEnviado,0);
-
-    if (contenidoChat !== null) {
-        contenidoChat.scrollTop = contenidoChat.scrollHeight;
+// Enviar un mensaje a la api
+async function enviarMensajeApi(mensaje){
+    try{
+        let data = await fetch('http://uwu-guate.site:3000/messages',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify(mensaje)
+        })    
+        if(!data.ok){
+            throw new Error(`HTTP error! status: ${data.status}`);
+        }
+        let posts = await data.json();
+        console.log(posts);
+    
+        return posts;
+    }catch(error){
+        console.error('Error:', error);
     }
-
-    escribirMensaje.value = '';
+    
 }
 
 //Función para animar los mensajes enviados
@@ -273,7 +354,6 @@ function animacionEntrada(texto,distancia) {
 }
 
 //Función para cambiar entre modo claro y oscuro
-
 let modoClaro = true;
 function cambiarModo(){
     modoClaro = !modoClaro;
@@ -299,14 +379,30 @@ function cambiarModo(){
     botonModo.style.backgroundColor = colores.botones;
     botonModo.style.color = colores.texto;
     botonModo.textContent = colores.leyendaBotonCambio;
-    let mensajesRecibidos = document.getElementsByClassName('mensaje-chat');
+    const mensajesRecibidos = document.getElementsByClassName('mensaje-chat');
     for (let i = 0; i < mensajesRecibidos.length; i++) {
         mensajesRecibidos[i].style.backgroundColor = colores.recibidos;
     }
-    let mensajesEnviados = document.getElementsByClassName('mensaje-enviado');
+    const mensajesEnviados = document.getElementsByClassName('mensaje-enviado');
     for (let i = 0; i < mensajesEnviados.length; i++) {
         mensajesEnviados[i].style.backgroundColor = colores.enviados;
     }
+}
+
+// Función para verificar que el link es de una imagen
+function verificarURL(url) {
+    const expresionRegularImagen = /\.(jpg|jpeg|png|gif)$/i;
+    return expresionRegularImagen.test(url);
+}
+
+// Función para filtrar mensajes según la búsqueda
+function filtrarMensajes(posts) {
+    const textoBusqueda = barraBusqueda.value.toLowerCase();
+
+    return posts.filter(post => {
+        return post.username.toLowerCase().includes(textoBusqueda) ||
+               post.content.toLowerCase().includes(textoBusqueda);
+    });
 }
 
 //Aplica el localstorage al cargar
@@ -316,7 +412,11 @@ window.onload = function() {
         modoClaro = modoGuardado === 'true';
         cambiarModo();
     }
+    console.log('Valor almacenado en localStorage:', modoGuardado);
 };
+
+// Hace el intervalo para el auto-refresher
+const autoRefresher = setInterval(actualizarContenido, 10000);
 
 //Acción del botón para mandar el mensaje
 botonEnviar.addEventListener('click', enviarMensaje)
@@ -332,4 +432,23 @@ escribirMensaje.addEventListener("keydown", function(event){
 //Acción del botón para cambiar modo entre claro y oscuro
 botonModo.addEventListener('click', cambiarModo);
 
-crearMensajes();
+// Acción para buscar chats
+let timeoutId;
+barraBusqueda.addEventListener('input', function () {
+    // Agrega un retraso antes de ejecutar la búsqueda para evitar la recarga excesiva
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+        mostrarChats();
+    }, 3000); // Ajusta el valor del tiempo de espera según sea necesario
+});
+
+// Función para mostrar los integrantes y chats
+async function actualizarContenido() {
+    mostrarIntegrantes();
+    mostrarChats();
+}
+
+// Evita que el refresher continue si la ventana se cierra
+window.addEventListener('beforeunload', () => {
+    clearInterval(autoRefresher);
+});
